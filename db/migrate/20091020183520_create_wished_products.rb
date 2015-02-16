842e81157c38104f800f453e2edd6431aa0bdd7e
ast0708 <ast0708@gmail.com>
2015-02-16 23:33:50 +0530

    change variants to suites

--- db/migrate/20091020183520_create_wished_products.rb
+++ db/migrate/20091020183520_create_wished_products.rb
@@ -1,7 +1,7 @@
 class CreateWishedProducts < ActiveRecord::Migration
   def self.up
     create_table :wished_products do |t|
-      t.references :variant
+      t.references :suites
       t.references :wishlist
       t.text :remark
 
